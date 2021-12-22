27731bebc8e00f7e8e0cab6cf629216193abcfbc
AleksApilats <shurikapilats@googlemail.com>
2021-12-22 16:01:15 +0000

    started working on testing unit

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 /* 
-👉 Write your kata here!
+👉 Kata is to sort courses array of objects in ascending order by using name key. 
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+
+const courses = [
+    {id: 1, name: "Node.js"},
+    {id: 2, name: "javaScript"},
+    {id: 3, name: "bash"},
+    {id: 4, name: "SQL"},
+    {id: 5, name: "TDD"},
+  ];// an array of objects
+
+
+  export function objectAscSorter(object) {
+  let sortedObjects = object.sort(function(a, b) {//creating a function within sort method with a and b parameters
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+  //making sure that function converts capital letters to lowercase that might be in key name so it wont affect the ASCII order
+    
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+  //here we create comparison logic so if if value of nameA is less than value of nameB then it goes before, if value if nameA is greater than nameB then it goes after nameB, otherwise they are equivalent to each other
+  });
+  return sortedObjects;
+}
+  console.log(objectAscSorter(courses));
--- main.test.js
+++ main.test.js
@@ -1,1 +1,19 @@
 //👉 Write your tests below here:
+import { objectAscSorter } from  "./main.js";
+
+const actual = [
+    {id: 1, name: "Node.js"},
+    {id: 2, name: "javaScript"},
+    {id: 3, name: "bash"},
+    {id: 4, name: "SQL"},
+    {id: 5, name: "TDD"},
+  ];
+const expected = [{id: 3, name: "bash"},
+    {id: 2, name: "javaScript"},
+    {id: 1, name: "Node.js"},  
+    {id: 4, name: "SQL"},
+    {id: 5, name: "TDD"}];
+test(`Sort array of objects by name in asc order`, () => {
+    
+    expect(actual).toEqual(expected)
+  });
